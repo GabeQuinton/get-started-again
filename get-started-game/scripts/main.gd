3c6203bc7a58f6cf478ff96207f0804e945c371d
aaahhhyyyuuuggg <danielmujtaba@gmail.com>
2025-07-14 15:10:10 -0500

    added an error screen when you click the main game

--- get-started-game/scripts/main.gd
+++ get-started-game/scripts/main.gd
@@ -2,16 +2,18 @@ extends Node2D
 
 
 @onready var options_menu: Panel = $OptionsMenu
+@onready var error_screen: Panel = $"Error Screen"
 
 func _ready() -> void:
 	options_menu.visible = false
+	error_screen.visible = false
 
 func _on_web_browser_pressed() -> void:
 	pass # Replace with function body.
 
 
 func _on_dragon_dating_sim_pressed() -> void:
-	pass # Replace with function body.
+	error_screen.visible = true
 
 
 func _on_file_finder_pressed() -> void:
@@ -28,3 +30,7 @@ func _on_exit_game_pressed() -> void:
 
 func _on_exit_menu_pressed() -> void:
 	options_menu.visible = false
+
+
+func _on_close_screen_pressed() -> void:
+	error_screen.visible = false
